efeea99f7e3139f4027cc55f53e568282e3b9b76
Hugo-24 <hugo.barrerog@udea.edu.co>
2026-08-22 18:28:46 -0500

    feat: add unit 6 practice banks

--- src/data/physics/family-registry.js
+++ src/data/physics/family-registry.js
@@ -10,6 +10,8 @@ import { UNIT_4_EXERCISE_FAMILIES } from "./unit-4/families.js";
 import { generateLocalizedUnit4FamilyInstance } from "./unit-4/family-localize.js";
 import { UNIT_5_EXERCISE_FAMILIES } from "./unit-5/families.js";
 import { generateLocalizedUnit5FamilyInstance } from "./unit-5/family-localize.js";
+import { UNIT_6_EXERCISE_FAMILIES } from "./unit-6/families.js";
+import { generateLocalizedUnit6FamilyInstance } from "./unit-6/family-localize.js";
 
 const FAMILY_REGISTRY = new Map([
   ...UNIT_1_EXERCISE_FAMILIES.map((family) => [family.id, { family, generate: generateLocalizedUnit1FamilyInstance }]),
@@ -17,6 +19,7 @@ const FAMILY_REGISTRY = new Map([
   ...UNIT_3_EXERCISE_FAMILIES.map((family) => [family.id, { family, generate: generateLocalizedUnit3FamilyInstance }]),
   ...UNIT_4_EXERCISE_FAMILIES.map((family) => [family.id, { family, generate: generateLocalizedUnit4FamilyInstance }]),
   ...UNIT_5_EXERCISE_FAMILIES.map((family) => [family.id, { family, generate: generateLocalizedUnit5FamilyInstance }]),
+  ...UNIT_6_EXERCISE_FAMILIES.map((family) => [family.id, { family, generate: generateLocalizedUnit6FamilyInstance }]),
 ]);
 
 export const getAcademicExerciseFamily = (id) => FAMILY_REGISTRY.get(id)?.family ?? null;
---
+++ src/data/physics/unit-6/bank.js
@@ -0,0 +1,4 @@
+import { UNIT_6_EXERCISES } from "./exercises.js";
+import { UNIT_6_EXERCISE_FAMILIES } from "./families.js";
+export { UNIT_6_EXERCISES, UNIT_6_EXERCISE_FAMILIES };
+export const UNIT_6_BANK_ITEMS=[...UNIT_6_EXERCISES,...UNIT_6_EXERCISE_FAMILIES];
